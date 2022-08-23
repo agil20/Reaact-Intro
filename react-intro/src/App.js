861846e84ce94a663820e9c8af02d1a218b9357b
agil20 <100156146+agil20@users.noreply.github.com>
2022-08-23 05:03:59 -0700

    first commit

---
+++ react-intro/src/App.js
@@ -0,0 +1,16 @@
+import User from './Components/User';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     
+
+       <User/>
+       <Navbar/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ react-intro/src/Components/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class User extends Component {
+  render() {
+    return (
+     <form>
+  <input type="text"/>
+  <button>Gonder</button>
+</form>
+
+    )
+  }
+}
